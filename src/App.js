c1800194215c7960c7f37311d20936525367ef58
freetimezp <kraftukrnet@ukr.net>
2023-07-14 22:33:47 +0300

    03. js-social-12-giffffy. Button Component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
+
 import { useTheme } from "./context/themeContext";
 import Header from "./Components/Header";
+import Button from "./Components/Button";
 
 function App() {
   const theme = useTheme();
@@ -9,6 +11,21 @@ function App() {
   return (
     <AppStyled theme={theme}>
       <Header />
+
+      <div className="fetch-btns">
+        <Button
+          name={"Liked"}
+          icon={<i className="fa-solid fa-heart"></i>}
+        />
+        <Button
+          name={"Trending Gifs"}
+          icon={<i className="fa-solid fa-arrow-trend-up"></i>}
+        />
+        <Button
+          name={"Random Gifs"}
+          icon={<i className="fa-solid fa-shuffle"></i>}
+        />
+      </div>
     </AppStyled>
   );
 }
@@ -16,6 +33,14 @@ function App() {
 const AppStyled = styled.div`
   min-height: 100vh;
   background-color: ${props => props.theme.colorBg1};
+
+  .fetch-btns {
+    display: flex;
+    justify-content: center;
+    gap: 4rem;
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+  }
 `;
 
 export default App;
